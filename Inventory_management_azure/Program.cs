6871c9e8590244ce0a4c98f56067ca91db2f1991
Ubuntu <ubuntu@ip-172-31-3-212.ap-south-1.compute.internal>
2026-06-18 14:22:21 +0000

    Fix project: proper NuGet packages, clean Program.cs, add host.json, remove decompiler artifacts

--- Inventory_management_azure/Program.cs
+++ Inventory_management_azure/Program.cs
@@ -1,18 +1,14 @@
-using System.Runtime.CompilerServices;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-[CompilerGenerated]
-internal class Program
-{
-	private static void _003CMain_003E_0024(string[] args)
-	{
-		new HostBuilder().ConfigureFunctionsWebApplication().ConfigureServices(delegate(IServiceCollection services)
-		{
-			services.AddApplicationInsightsTelemetryWorkerService();
-			services.ConfigureFunctionsApplicationInsights();
-		}).Build()
-			.Run();
-	}
-}
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services =>
+    {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+    })
+    .Build();
+
+host.Run();
